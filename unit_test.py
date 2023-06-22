75d4b46e3956e25b51b3e0dde21b03ee2d1ce393
Arthur Lee <arthur@fyne-llc.com>
2023-06-22 16:28:34 -0400

    Fixed for missing query2

--- unit_test.py
+++ unit_test.py
@@ -15,12 +15,22 @@ def unit_test_on_query_to_create_updated_table():
     FROM `{target_project_id}.{dataset_id}.{table_id}`
     """
 
+    query2 = f"""
+    SELECT COUNT(*) as total_row_count
+    FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY source_key ORDER BY source_creation_time DESC) AS row_num
+        FROM `{target_project_id}.{dataset_id}.{table_id}`
+    ) t
+    WHERE row_num = 1
+    """
+
     query_job1 = client.query(query1)
     results1 = query_job1.result()
 
     for row in results1:
         original_row_count = row.row_count
-        print(f"Row count from `test1` table: {original_row_count}")
+        print(f"Row count from `{table_id}` table: {original_row_count}")
 
     query_job2 = client.query(query2)
     results2 = query_job2.result()
