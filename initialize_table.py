19467704fec0a33452e00076d1d8437a9f3d4d44
Arthur Lee <arthur@fyne-llc.com>
2023-06-23 11:05:50 -0400

    Updated for added columns

--- initialize_table.py
+++ initialize_table.py
@@ -22,16 +22,14 @@ def initialize_table_for_incremental():
         table_exists = False
 
     if table_exists:
-        delete_query = f"""
-            DELETE
-            FROM `{target_project_id}.{target_dataset}.{target_table}`
-            WHERE TRUE
+        truncate_query = f"""
+            TRUNCATE TABLE `{target_project_id}.{target_dataset}.{target_table}`
         """
-        client.query(delete_query).result()
+        client.query(truncate_query).result()
 
         insert_query = f"""
             INSERT INTO `{target_project_id}.{target_dataset}.{target_table}`
-            SELECT *
+            SELECT *, NULL AS source_table_name, NULL AS source_creation_time
             FROM `{source_project_id}.{source_dataset}.{source_table}`
         """
         client.query(insert_query).result()
@@ -42,3 +40,36 @@ def initialize_table_for_incremental():
             FROM `{source_project_id}.{source_dataset}.{source_table}`
         """
         client.query(create_query).result()
+        
+        # Define the new columns
+        new_columns = [
+            {"name": "source_table_name", "field_type": "STRING", "mode": "NULLABLE"},
+            {"name": "source_creation_time", "field_type": "TIMESTAMP", "mode": "NULLABLE"}
+        ]
+
+        # Retrieve the existing table
+        table = client.get_table(table_ref)
+
+        # Add new columns to the schema
+        table.schema = list(table.schema) + [bigquery.SchemaField(name=column['name'], field_type=column['field_type'], mode=column['mode']) for column in new_columns]
+
+        # Update the table with the new schema
+        client.update_table(table, ["schema"])
+
+    # Compare the number of rows in the source and target tables
+    source_count_query = f"""
+        SELECT COUNT(*)
+        FROM `{source_project_id}.{source_dataset}.{source_table}`
+    """
+    target_count_query = f"""
+        SELECT COUNT(*)
+        FROM `{target_project_id}.{target_dataset}.{target_table}`
+    """
+    source_count = client.query(source_count_query).result().total_rows
+    target_count = client.query(target_count_query).result().total_rows
+
+    # Output the result
+    if source_count == target_count:
+        print('Row count matches in source and target tables.')
+    else:
+        print('Row count mismatch between source and target tables.')
